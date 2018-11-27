3223111379c2ae3021caa3fb39c94fdb819e323f
Xin Zhang <zxin1112@gmail.com>
2018-11-27 15:28:34 +0800

    add Event

---
+++ 委托和事件/事件/EvevtTest/Custum/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//自定义事件
+namespace Custum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Incrementer incrementer = new Incrementer();//发布者
+            Dozens dozens = new Dozens(incrementer);//订阅者
+            incrementer.DoCount();//满足条件时触发事件
+            Console.WriteLine("Number：{0}", dozens.DozensCount);
+            Console.ReadKey();
+        }
+    }
+    class IncrementerEventArgs: EventArgs//自定义类,派生自EventArgs
+    {
+        public int IterationCount { get;  set; }
+    }
+    class Incrementer//发布者
+    {
+        public event EventHandler<IncrementerEventArgs> CountedADozen;//使用自定义类的泛型委托
+        public void DoCount()
+        {
+            IncrementerEventArgs eventArgs = new IncrementerEventArgs();//自定义类对象
+            for(int i=1;i<100;i++)
+                if (i % 12 == 0 && CountedADozen != null)
+                {
+                    eventArgs.IterationCount = i;
+                    CountedADozen(this, eventArgs);//触发事件
+                }
+        }
+    }
+    class Dozens//订阅者类
+    {
+        public int DozensCount { get; private set; }
+        public Dozens(Incrementer incrementer)
+        {
+            DozensCount = 0;
+            incrementer.CountedADozen += IncrementDozensCount ;//注册事件
+        }
+        void IncrementDozensCount(object s,IncrementerEventArgs e)//处理程序
+        {
+            DozensCount++;
+            Console.WriteLine("Incremented {0} in {1}", e.IterationCount, s.ToString());
+        }
+    }
+}
---
+++ 委托和事件/事件/EvevtTest/EvevtTest/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//标准事件
+namespace EvevtTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Incrementer incrementer = new Incrementer();//发布者
+            Dozens dozens = new Dozens(incrementer);//订阅者
+            incrementer.DoCount();//执行方法，达成一定条件触发事件
+            Console.WriteLine("Number：{0}", dozens.Dozensount);//触发的事件改变了订阅者类中的属性
+            Console.ReadKey();
+        }
+    }
+    class Incrementer//发布者
+    {
+       public delegate void Handler();
+        public event Handler Event_Test;//自定义委托类型的事件
+        public event EventHandler CountedADozen;//系统定义委托的事件
+        public void DoCount()
+        {
+            for (int i = 1; i < 100; i++)
+                if (i % 12 == 0 && CountedADozen != null)
+                {//每计数12后触发一次事件，触发事件前先确定是否注册
+                    CountedADozen(this, null);
+                    Event_Test();
+                }
+        }
+    }
+    class Dozens//订阅者
+    {
+        public int Dozensount { get; private set; }
+        public Dozens(Incrementer incrementer)//构造函数
+        {
+            Dozensount = 0;
+            incrementer.CountedADozen += IncrementDozensCount;//订阅事件
+            incrementer.Event_Test += () =>
+            
+                Console.WriteLine("Enevt_Test");
+            //Lambda表达式订阅的事件
+        }
+        void IncrementDozensCount(object s,EventArgs e)//事件处理程序
+        {
+            Dozensount++;
+        }
+    }
+}
